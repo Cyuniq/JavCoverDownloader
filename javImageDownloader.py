58e08508cf1a14e8abfddfe020b1f173721dd5da
cyuniq <c515227473@gmail.com>
2022-06-17 20:15:16 +0800

    增加文件名无效信息过滤函数

--- javImageDownloader.py
+++ javImageDownloader.py
@@ -5,16 +5,13 @@ import re
 import os   #用于读写文件操作
 import time #用于计算运行时间
 
+#从网页抓取指定信息
 def getInfo(frontname):
-    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}
+    global headers
     #网站需要挂代理
-    proxy = '127.0.0.1:10808'
-    proxies = {
-        'http': 'socks5://' + proxy,
-        'https': 'socks5://' + proxy,
-    }
+    global proxies
     mainUrl="https://www.javlibrary.com/cn/vl_searchbyid.php?keyword="+frontname
-    response = requests.get(mainUrl,proxies=proxies, headers = headers)
+    response = requests.get(mainUrl,proxies = proxies, headers = headers)
 
     soup = BeautifulSoup(response.text,'lxml')
     global stateCode
@@ -24,6 +21,7 @@ def getInfo(frontname):
     if videoTitle[27].get('title') == "我想要" :
         print('-! ! ! ! ! ! ! ! ! ! ! ! ! ! !')
         print("-查询成功:"+frontname+"存在多个返回结果，请手动处理")
+        #print(soup.prettify())
         stateCode = 1
         return
     if videoTitle[27].string == "tt-01sd3" :
@@ -42,12 +40,8 @@ def getInfo(frontname):
             print(e)
 
 def downloadImage(name,url):
-    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}
-    proxy = '127.0.0.1:10808'
-    proxies = {
-        'http': 'socks5://' + proxy,
-        'https': 'socks5://' + proxy,
-    }
+    global headers
+    global proxies
     if url[0:6] == "https:":
         jpgUrl=str(url)
     else :
@@ -75,23 +69,59 @@ def downloadImage(name,url):
     except requests.exceptions.RequestException as e:
         print(e)
 
+def strName(name):
+    name=name.replace('-A','')
+    name=name.replace('-B','')
+    name=name.replace('-C','')
+    name=name.replace('-D','')
+    name=name.replace('-2k','')
+    name=name.replace('-4k','')
+    name=name.replace('ch','')
+    name=name.replace('jav20s8.com@','')
+    name=name.replace('hhd800.com@','')
+    
+    name=name.upper()
+    return name
 
 
 
-stateCode = 0 # 0-状态正常 1-多个返回结果 2-不存在搜索结果
-path=os.getcwd() #获取当前文件夹路径
-fileList=os.listdir(path) #获取当前目录下文件列表
+
+#全局变量部分
+
+#状态码
+# 0-状态正常 1-多个返回结果 2-不存在搜索结果
+stateCode = 0 
+
+#代理配置
+proxyProtocol = 'socks5'
+proxyHost     = '127.0.0.1'
+proxyPort     = '10808'
+proxies = {
+    'http':  proxyProtocol+'://'+proxyHost+':'+proxyPort,
+    'https': proxyProtocol+'://'+proxyHost+':'+proxyPort,
+}
+
+headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}
+
+
+
+#获取当前文件夹路径
+path=os.getcwd()
+#获取当前目录下文件列表
+fileList=os.listdir(path) 
 
 '''
 #####测试用代码块#####
-fileName="stars-461.mp4"
+fileName="tek-077-C.mp4"
+print(strName(fileName))
 frontName=os.path.splitext(fileName)[0]
+strfrontName=strName(frontName)
 #print(frontName)
 backNmae=os.path.splitext(fileName)[1]
 #print(backNmae)
 ctcStart=time.time()
 print('------------------------------')
-getInfo(frontName)
+getInfo(strfrontName)
 if stateCode == 0 :
     downloadImage(videoTitle[27].string,pic.get('src'))
 ctcEnd=time.time()
@@ -100,7 +130,7 @@ print("-耗    时:{:.2f}秒".format(ctcEnd-ctcStart))
 '''
 
 
-
+'''
 for fileName in fileList:
     frontName=os.path.splitext(fileName)[0]
     #print(frontName)
@@ -108,8 +138,11 @@ for fileName in fileList:
     #print(backNmae)
     ctcStart=time.time()
     print('------------------------------')
-    getInfo(frontName)
+    strfrontName=strName(frontName)
+    print('-识别番号:'+strfrontName)
+    getInfo(strfrontName)
     if stateCode == 0 :
         downloadImage(videoTitle[27].string,pic.get('src'))
     ctcEnd=time.time()
     print("-耗    时:{:.2f}秒".format(ctcEnd-ctcStart))
+'''
